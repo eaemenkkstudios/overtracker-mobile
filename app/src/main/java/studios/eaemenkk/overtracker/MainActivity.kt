9a52fccbc852abd608904fa7329da45a3e7ad076
cdias900 <pedro@cdias.dev>
2020-04-14 21:44:06 -0300

    Initial commit

---
+++ app/src/main/java/studios/eaemenkk/overtracker/MainActivity.kt
@@ -0,0 +1,12 @@
+package studios.eaemenkk.overtracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
