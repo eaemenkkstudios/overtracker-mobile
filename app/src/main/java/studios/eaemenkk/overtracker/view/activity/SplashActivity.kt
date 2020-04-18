fd86e11e738c7d68cc5e064635869c2e8a92cdcb
cdias900 <pedro@cdias.dev>
2020-04-18 15:43:37 -0300

    Adiciona splash screen

---
+++ app/src/main/java/studios/eaemenkk/overtracker/view/activity/SplashActivity.kt
@@ -0,0 +1,24 @@
+package studios.eaemenkk.overtracker.view.activity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.Window
+import android.view.WindowManager
+import studios.eaemenkk.overtracker.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.requestFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            startActivity(Intent(this@SplashActivity, LoginActivity::class.java))
+            finish()
+        },2000)
+    }
+}
