ae91f71b22db212bba70862246c6f5b3c295b506
Thiago <thiagozoid@gmail.com>
2020-04-18 13:13:19 -0300

    Card de atualização de SR

--- app/src/main/java/studios/eaemenkk/overtracker/view/adapter/CardAdapter.kt
+++ app/src/main/java/studios/eaemenkk/overtracker/view/adapter/CardAdapter.kt
@@ -3,6 +3,7 @@ package studios.eaemenkk.overtracker.view.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.feed_list_item.view.*
@@ -21,14 +22,25 @@ class CardAdapter(private val dataSet: Array<Card>): RecyclerView.Adapter<CardAd
 
     override fun onBindViewHolder(holder: CardViewHolder, position: Int) {
         val card = dataSet[position]
-        holder.tag.text = card.player?.tag
-        holder.platform.text = card.player?.platform
-        holder.type.text = card.type
+        val battleTag = card.player?.tag?.split("#")
+        holder.tag.text = battleTag?.get(0)
+        holder.tagNum.text = "#${battleTag?.get(1)}"
+        holder.platform.text = card.player?.platform?.toUpperCase()
+        holder.previous.text = card.sr?.previous;
+        holder.current.text = card.sr?.current;
+        when (card.role) {
+            "support" -> holder.role.setImageResource(R.drawable.support)
+            "damage" -> holder.role.setImageResource(R.drawable.damage)
+            "tank" -> holder.role.setImageResource(R.drawable.tank)
+        }
     }
 
     class CardViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
-        val tag: TextView = itemView.tvTagNum
+        val tag: TextView = itemView.tvTag
+        val tagNum: TextView = itemView.tvTagNum
         val platform: TextView = itemView.tvPlatform
-        val type: TextView = itemView.tvRole
+        val role: ImageView = itemView.ivRole
+        val previous: TextView = itemView.tvSrPrevious
+        val current: TextView = itemView.tvSrCurrent
     }
 }
